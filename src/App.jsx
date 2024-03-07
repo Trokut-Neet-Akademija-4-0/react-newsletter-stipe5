2130f23bc29f7f1ea4dfc16c08e27d498674ec2a
Jasmin Causevic <jcausevi@gmail.com>
2024-03-07 16:18:46 +0100

    Starting point for practice Newsletter

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,18 @@ import './App.css'
 import { Newsletter } from './Newsletter.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      {/*<div>*/}
-      {/*  /!*<a href="https://vitejs.dev" target="_blank">*!/*/}
-      {/*  /!*  <img src={viteLogo} className="logo" alt="Vite logo" />*!/*/}
-      {/*  /!*</a>*!/*/}
-      {/*  /!*<a href="https://react.dev" target="_blank">*!/*/}
-      {/*  /!*  <img src={reactLogo} className="logo react" alt="React logo" />*!/*/}
-      {/*  /!*</a>*!/*/}
-      {/*</div>*/}
-      {/*<h1>Vite + React</h1>*/}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 style={{color: 'white'}}>Vite + React</h1>
       <Newsletter />
     </>
   )
